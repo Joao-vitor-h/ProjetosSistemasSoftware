da362296378794ce6baf89db0c8d967827292274
João Vitor <162624445+Joao-vitor-h@users.noreply.github.com>
2026-01-20 09:10:18 -0300

    Update Permissoes.java

--- src/main/java/com/atividade/model/Permissoes.java
+++ src/main/java/com/atividade/model/Permissoes.java
@@ -1,1 +1,31 @@
+package com.atividade.model;
 
+// @author JoaoVitor
+
+public abstract class Permissoes implements RegraUsuarioAdministradorEvoluido, RegraUsuarioAdministradorPadrao {
+      protected Usuario usuario;
+
+      public Permissoes(Usuario usuario) {
+            this.usuario = usuario;
+      }
+
+      @Override
+      public void enviarMensagem(UsuarioRepository repository) {
+          throw new RuntimeException("Não é possível enviar mensagem.");
+      }
+      
+      @Override
+      public void autorizarUsuario(Usuario usuario) {
+          throw new RuntimeException("Não é possível autorizar usuario.");
+      }
+
+      @Override
+      public void evoluirUsuario(Usuario usuario) {
+          throw new RuntimeException("Não é possível evoluir usuario.");
+      }
+      
+      @Override
+      public void rebaixarUsuario(Usuario usuario) {
+          throw new RuntimeException("Não é possível rebaixar usuario.");
+      }
+}
